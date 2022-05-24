81669c849ab38fc7b3dcc40a2986dc617a3c806c
Festyflux <ewansihafestus@gmail.com>
2022-05-24 13:35:32 +0100

    Wrote a few codes on app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 //I have modified this file.
+
+global.setTimeout (() => {
+    console.log ('I set a timeout for 3 seconds');
+}, 3000);
+
+setInterval (() => {
+    console.log("Then, I reoccur every 5 seconds, till infinity");
+}, 5000);
+
+setInterval (() => {
+    console.log('To stop me, press ctr + C');
+}, 7000);
+
