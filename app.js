8c0ea9dcaf2c810268add153fba4a27172d0428c
Festyflux <ewansihafestus@gmail.com>
2022-05-24 13:38:50 +0100

    Added comments to app.js

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 //I have modified this file.
 
-global.setTimeout (() => {
-    console.log ('I set a timeout for 3 seconds');
-}, 3000);
+// global.setTimeout (() => {
+//     console.log ('I set a timeout for 3 seconds');
+// }, 3000);
 
+//Set interval for 5 seconds
 setInterval (() => {
     console.log("Then, I reoccur every 5 seconds, till infinity");
 }, 5000);
 
+
+//Set intervals for 7 Seconds
 setInterval (() => {
     console.log('To stop me, press ctr + C');
 }, 7000);
+
+
 
